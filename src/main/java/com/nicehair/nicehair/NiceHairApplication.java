3564ea1ecbeb15cf9334d403cb720dcd59652882
iamducthanh <65590543+iamducthanh@users.noreply.github.com>
2024-05-31 22:03:16 +0700

    up

---
+++ src/main/java/com/nicehair/nicehair/NiceHairApplication.java
@@ -0,0 +1,13 @@
+package com.nicehair.nicehair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceHairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiceHairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicehair/nicehair/NiceHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicehair.nicehair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceHairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
